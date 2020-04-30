d4735a47bbfe09f7a008e08e90173e161166466e
seonukim <92.seoonooo@gmail.com>
2020-04-30 17:54:18 +0900

    committed in vscode

---
+++ machine_learning/Perceptron_iris.py
@@ -0,0 +1,102 @@
+#1. 패키지 호출
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+#2. Perceptron 클래스 구현
+class Perceptron(object):
+    def __init__(self, eta = 0.01, n_iter = 50, random_state = 1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc = 0.0, scale = 0.01,
+                              size = 1 + X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+#3. Iris 데이터 불러오기
+df = pd.read_csv('https://raw.githubusercontent.com/gilbutITbook/007022/master/code/ch02/iris.data', header = None)
+
+print(df.head())
+
+#4. 퍼셉트론 분류기 훈련
+# setosa와 versicolor를 선택
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+
+# 꽃받침 길이와 꽃잎 길이를 추출
+X = df.iloc[0:100, [0, 2]].values
+
+# 산점도 그리기
+plt.scatter(X[:50, 0], X[:50, 1],
+            color = 'red', marker = 'o', label = 'setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1],
+            color = 'blue', marker = 'x', label = 'versicolor')
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc = 'upper left')
+plt.show()
+
+# 퍼셉트론 에포크 대비 잘못 분류된 오차
+ppn = Perceptron(eta = 0.1, n_iter = 10)
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) + 1),
+         ppn.errors_, marker = 'o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of errors')
+plt.show()
+
+# 컬러맵 함수 정의
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution = 0.02):
+    
+    # 마커와 컬러맵 설정하기
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # 결정 경계 그리기
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contour(xx1, xx2, Z, alpha = 0.3, cmap = cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # 샘플의 산점도 그리기
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x = X[y == cl, 0],
+                    y = X[y == cl, 1],
+                    alpha = 0.8,
+                    c = colors[idx],
+                    marker = markers[idx],
+                    label = cl,
+                    edgecolor = 'black')
+
+# 결정 경계 그래프
+plot_decision_regions(X, y, classifier = ppn)
+plt.xlabel('sepal lenght [cm]')
+plt.ylabel('patal length [cm]')
+plt.legend(loc = 'upper left')
+plt.show()
