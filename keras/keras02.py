92d5ffe92153d5cff714e5ed89f3fbb7ad83036c
seonukim <92.seoonooo@gmail.com>
2020-05-12 14:20:10 +0900

    modified

--- keras/keras02.py
+++ keras/keras02.py
@@ -13,10 +13,10 @@ y_test = np.array([101, 102, 103, 104, 105, 106, 107, 108, 109, 110])
 model = Sequential()
 model.add(Dense(5, input_dim = 1, activation = 'relu'))
 model.add(Dense(3))
-model.add(Dense(4))
-model.add(Dense(2))
 model.add(Dense(1, activation = 'relu'))
+model.summary()
 
+"""
 model.summary()
 
 # 4. 컴파일하기
@@ -38,3 +38,4 @@ print("acc : ", acc)
 # 7. 결과물 확인
 output = model.predict(x_test)
 print("결과물 : \n", output)
+"""
