c0cbe71d7bbf28cad4981f0ba4d87f6b3a5852c8
seonukim <92.seoonooo@gmail.com>
2020-05-12 11:31:12 +0900

    adjust the number of nodes in keras02_1.py

--- keras/keras02_1.py
+++ keras/keras02_1.py
@@ -12,14 +12,14 @@ y_test = np.array([202, 204, 206, 208, 210, 212, 214, 216, 218, 220])
 # 3. 모델 구성
 model = Sequential()
 model.add(Dense(5, input_dim = 1, activation = 'relu'))
-model.add(Dense(20))
-model.add(Dense(15))
-model.add(Dense(13))
-model.add(Dense(10))
-model.add(Dense(7))    # 레이어 깊이 조절
-model.add(Dense(6))    # 레이어 깊이 조절
-model.add(Dense(4))    # 레이어 깊이 조절
-model.add(Dense(3))
+model.add(Dense(200))
+model.add(Dense(153))
+model.add(Dense(131))
+model.add(Dense(115))
+model.add(Dense(79))    # 레이어 깊이 조절
+model.add(Dense(64))    # 레이어 깊이 조절
+model.add(Dense(43))    # 레이어 깊이 조절
+model.add(Dense(34))    # 노드 조절
 model.add(Dense(1, activation = 'relu'))
 
 model.summary()
