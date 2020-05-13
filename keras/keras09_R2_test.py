886941b3ad1180b538691e3cdabc4ab4dfb871dc
seonukim <92.seoonooo@gmail.com>
2020-05-13 19:19:09 +0900

    assignment 20200513

---
+++ keras/keras09_R2_test.py
@@ -0,0 +1,57 @@
+"""
+2020.05.13 과제
+R2를 음수가 아닌 0.5 이하로 줄이기.
+레이어는 인풋과 아웃풋을 포함하여 5개 이상,
+노드는 레이어 당 각각 5개 이상
+batch_size = 1
+epochs = 100 이상
+"""
+
+# 1. 데이터
+import numpy as np
+x_train = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+y_train = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+x_test = np.array([11, 12, 13, 14, 15])
+y_test = np.array([11, 12, 13, 14, 15])
+x_pred = np.array([16, 17, 18])
+
+# 2. 모델 구성
+from keras.models import Sequential
+from keras.layers import Dense
+model = Sequential()
+
+model.add(Dense(5, input_dim = 1, activation='hard_sigmoid'))
+model.add(Dense(5))
+model.add(Dense(5))
+model.add(Dense(1))
+
+# 3. 훈련
+model.compile(loss = 'mse', metrics = ['mse'], optimizer = 'adam')
+model.fit(x_train, y_train, epochs = 100, batch_size = 1, validation_data = (x_test, y_test))
+
+# 4. 평가 및 예측
+loss, mse = model.evaluate(x_test, y_test, batch_size = 1)
+print("loss : ", loss)
+print("mse : ", mse)
+
+y_predict = model.predict(x_test)
+print(y_predict)
+
+# 5. RMSE 구하기
+from sklearn.metrics import mean_squared_error
+def RMSE(y_test, y_predict):
+    return np.sqrt(mean_squared_error(y_test, y_predict))
+print("RMSE : ", RMSE(y_test, y_predict))
+
+# 6. R2 구하기
+from sklearn.metrics import r2_score
+r2 = r2_score(y_test, y_predict)
+print("R2 : ", r2)
+
+
+"""
+훈련 데이터와 같은 데이터로 평가를 했을 때 : 0.82
+compile의 parameter 중 loss를 'binary_crossentropy'로 했을 때 : 0.07 -> binary는 1, 0의 이진분류법인데
+회귀 모델에 적용하니 설명력이 낮게 나오는 것 같다.
+activation의 인자값을 sigmoid 계열로 입력 시 -> sigmoid계열 역시 분류함수인데 회귀 모델에 적용..
+"""
