f0a9e31be4b9a86bc04e932ca8fdc90f620e08f7
seonukim <92.seoonooo@gmail.com>
2020-05-19 16:10:18 +0900

    basic python

---
+++ python/python01.py
@@ -0,0 +1,69 @@
+# # 정수형
+
+# a = 1
+# b = 2
+# c = a + b
+
+# print(c)
+
+# d = a * b
+# print(d)
+
+# e = a / b
+# print(e)
+
+# # 실수형
+# a = 1.1
+# b = 2.2
+# c = a + b     #3.3000000000000003
+# print(c)
+
+# d= a * b #2.4200000000000004
+# print(d)
+
+# e = a / b
+# print(e)
+
+# a='hel'
+# b='lo'
+# c = a + b
+# print(c)
+
+a = 123
+b = '45'
+
+# c = a+b  # 타입 에러
+# print(c)
+
+# 숫자를 문자로 변환 + 문자
+# a = str(a)
+# print(a)
+
+# b = int(b)
+# c = a+b
+# print(c)
+
+# # 문자열 연산하기 
+# a = 'abcdefgh'
+# print(a[0])
+# print(a[3])
+# print(a[5])
+# print(a[-1])
+# print(a[-2])
+# print(type(a))
+
+# b = 'xyz'
+
+# print(a+b)
+
+# 문자열 인덱싱
+
+a = 'Hello, Deep learning'
+print(a[7])
+print(a[-1])
+print(a[-2])
+print(a[3:9]) #index 3~9까지에서 3, 9 제외
+print(a[3:-5]) # => 마지막꺼 제외 => a[-5] = r
+print(a[:-1]) # 처음부터 a[=1] 까지
+print(a[1:]) # a[1]부터 마지막까지
+print(a[5:-4])
---
+++ python/python02_print.py
@@ -0,0 +1,20 @@
+# print 문과 format 함수
+a = '사과'
+b = '배'
+c = '옥수수'
+
+print('명석이는 잘생겼다.')
+
+print(a)
+print(a,b)
+print(a,b,c)
+
+print("나는 {0}를 먹었다.".format(a)) # why?  왜 이런 방식
+print("나는 {0}와 {1}를 먹었다.".format(a,b)) # why?  왜 이런 방식
+print("나는 {0}와 {1}와 {2}를 먹었다.".format(a,b,c)) # why?  왜 이런 방식
+
+
+
+print('나는', a+'를  먹었다.')
+print('나는',a+'와', b+'를 먹었다.') # why?  왜 이런 방식
+print('나는' ,a+'와', b+'와', c+'를 먹었다.') # why?  왜 이런 방식
