5832fc40544930e3d3f8e89ad7c31dac8ab081b8
seonukim <92.seoonooo@gmail.com>
2020-05-22 15:38:25 +0900

    sklearn_MinMaxScaler

--- keras/keras38_minmax.py
+++ keras/keras38_minmax.py
@@ -1,37 +1,49 @@
 # keras38_minmax.py
-# EarlyStopping을 적용하여 함수형 모델을 리모델링하시오
+# scikit_learn의 표준화, 정규화
 
 # 1. 모듈 임포트
 from keras.models import Sequential, Model
 from keras.layers import Dense, LSTM, Input, Flatten
 from keras.callbacks import EarlyStopping
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 
-# 1-1. EarlyStopping 객체 생성
+# 1-1. 객체 생성
 early = EarlyStopping(monitor = 'loss', mode = 'min', patience = 10)
+scaler = MinMaxScaler()
+standard = StandardScaler
 
 
 # 2. 데이터 준비
+# 전처리는 x만 한다.
 x = np.array([[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6],
               [5, 6, 7], [6, 7, 8], [7, 8, 9], [8, 9, 10],
               [9, 10, 11], [10, 11, 12],
-              [20, 30, 40], [30, 40, 50], [40, 50, 60]])
-y = np.array([4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 50, 60, 70])
+              [2000, 3000, 4000], [3000, 4000, 5000], [4000, 5000, 6000],
+              [100, 200, 300]])
+y = np.array([4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 5000, 6000, 7000, 400])
 
-x_predict = np.array([50, 60, 70])
+x_predict = np.array([55, 65, 75])
 
-print("x의 차원 : ", x.shape)
-print("y의 차원 : ", y.shape)
-print("x 예측값의 차원 : ", x_predict.shape)
+scaler.fit(x)   # 실행하다
+x = scaler.transform(x)
+print(x)
+
+
+print("x의 차원 : ", x.shape)                   # (14, 3)
+print("y의 차원 : ", y.shape)                   # (14, )
+print("x_predict 차원 : ", x_predict.shape)    # (3, )
+
 
 # 2-1. 입력 데이터 reshape
-x = x.reshape(13, 3, 1)
+x = x.reshape(14, 3, 1)
 x_predict = x_predict.reshape(1, 3, 1)
 
 # print("x_reshape : ", x.shape)
 # print("x_predict_reshape : ", x_predict.shape)
 
-
+'''
 # 3. 모델 구성
 # LSTM(return_sequences) _ Sequential 모델
 model = Sequential()
@@ -53,7 +65,7 @@ model.add(Dense(1))
 
 model.summary()
 
-'''
+
 # LSTM(return_sequences) _ 함수형 모델
 input1 = Input(shape = (3, 1))
 dense1 = LSTM(51, return_sequences = True)(input1)
@@ -73,7 +85,7 @@ output6 = Dense(1)(output5)
 
 model = Model(inputs = input1,
               outputs = output6)
-'''
+
 
 # 4. 실행
 model.compile(loss = 'mse', metrics = ['mse'], optimizer = 'adam')
@@ -86,3 +98,4 @@ y_predict = model.predict(x_predict)
 
 print(x_predict)
 print(y_predict)
+'''
