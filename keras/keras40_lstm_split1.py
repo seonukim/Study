98c2b6cbd540623fc1e403bf9a12d1e67c7c52e8
seonukim <92.seoonooo@gmail.com>
2020-05-25 11:46:29 +0900

    add Result

--- keras/keras40_lstm_split1.py
+++ keras/keras40_lstm_split1.py
@@ -48,14 +48,20 @@ print(y)
 input1 = Input(shape = (4, 1))
 dense1 = LSTM(10, activation = 'relu', return_sequences = True)(input1)
 dense2 = LSTM(10, activation = 'relu', return_sequences = True)(dense1)
-dense3 = LSTM(8, activation = 'relu', return_sequences = True)(dense2)
-dense4 = LSTM(8, activation = 'relu', return_sequences = False)(dense3)
+dense3 = LSTM(8, activation = 'relu')(dense2)
+dense4 = Dense(8, activation = 'relu')(dense3)
+dense5 = Dense(7, activation = 'relu')(dense4)
+dense6 = Dense(9, activation = 'relu')(dense5)
 
-output1 = Dense(8)(dense4)
-output2 = Dense(7)(output1)
-output3 = Dense(1)(output2)
+output1 = Dense(8)(dense6)
+output2 = Dense(9)(output1)
+output3 = Dense(10)(output2)
+output4 = Dense(15)(output3)
+output5 = Dense(3)(output4)
+output6 = Dense(2)(output5)
+output7 = Dense(1)(output6)
 
-model = Model(inputs = input1, outputs = output3)
+model = Model(inputs = input1, outputs = output7)
 
 model.summary()
 
@@ -75,5 +81,39 @@ print("=" * 40)
 print("y_predict : \n", y_predict)
 
 
-# loss :  0.00020573670917656273
-# mse  :  0.00020573670917656273
+'''
+Result 1)
+loss :  0.002710927976295352
+mse  :  0.002710927976295352
+y_predict :
+ [[5.0754447]
+  [5.943391 ]
+  [6.948891 ]
+  [7.9733124]
+  [8.987036 ]
+  [9.937738 ]]
+
+
+Result 2)
+loss :  8.886436262400821e-05
+mse  :  8.886436262400821e-05
+y_predict :
+ [[ 5.0073833]
+  [ 5.9913664]
+  [ 7.0102158]
+  [ 8.007418 ]
+  [ 9.012097 ]
+  [10.009922 ]]
+
+
+Result 3)
+loss :  0.015172582119703293
+mse  :  0.015172582119703293
+y_predict :
+ [[5.103075 ]
+  [5.9094715]
+  [6.9401693]
+  [8.013643 ]
+  [8.984726 ]
+  [9.738817 ]]
+'''
