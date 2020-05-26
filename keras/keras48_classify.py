0d014580c1ff039170aee3dc2fd58582eaf82e45
seonukim <92.seoonooo@gmail.com>
2020-05-26 10:14:12 +0900

    add comment

--- keras/keras48_classify.py
+++ keras/keras48_classify.py
@@ -15,7 +15,7 @@ es = EarlyStopping(monitor = 'loss', mode = 'min', patience = 10)
 x = np.array(range(1, 11))
 y = np.array([1, 0 ,1, 0, 1, 0, 1, 0, 1, 0])
 print(x.shape)
-
+print(y.shape)
 
 # 3. 모델 구성
 model = Sequential()
@@ -24,15 +24,25 @@ model.add(Dense(10))
 model.add(Dense(13))
 model.add(Dense(12))
 model.add(Dense(1, activation = 'sigmoid'))
-
-# input1 = Input(shape = ())
-
+'''
+분류모형의 활성화 함수 - 마지막 아웃풋 레이어에 추가
+1. sigmoid
+2. hard_sigmoid
+3. softmax
+'''
 model.summary()
 
 # 4. 실행 및 훈련
 model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['acc'])
 model.fit(x, y, epochs = 1000,
           batch_size = 1, callbacks = [es])
+'''
+분류모형의 손실함수
+1. Cross-Entropy Loss
+2. Categorical Cross-Entropy Loss
+3. Binary Cross-Entropy Loss
+4. Focal loss (함수로 구현해서 사용)
+'''
 
 # 5. 평가 및 예측
 loss, acc = model.evaluate(x, y)
@@ -59,3 +69,17 @@ print("pred : \n", pred)
 #         return -K.mean(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))
 #                         - K.mean((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
 #     return focal_loss_fixed
+
+'''
+pred :
+ [[0.52978766]
+  [0.5066741 ]
+  [0.483532  ]
+  [0.4604602 ]
+  [0.43755686]
+  [0.41491616]
+  [0.39262962]
+  [0.3707816 ]
+  [0.34944952]
+  [0.32870376]]     # 0.5를 기준으로 0, 1로 분류
+'''
