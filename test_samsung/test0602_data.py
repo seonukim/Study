2186f3044153687f27ee867919eb2a7db7e4181a
seonukim <92.seoonooo@gmail.com>
2020-06-03 14:42:45 +0900

    add file

--- test_samsung/test0602_data.py
+++ test_samsung/test0602_data.py
@@ -1,38 +1,84 @@
-# 2020.06.02 시험 리뷰
-
 import numpy as np
-import pandas as pd
+import pandas as pd 
 
 samsung = pd.read_csv('./data/csv/samsung.csv',
-                      index_col = 0,
-                      header = 0,
+                      index_col = 0,    #None
+                      header = 0, 
                       sep = ',',
-                      encoding = 'cp949')
+                      encoding = 'CP949')
+                       
+hite = pd. read_csv('./data/csv/hite.csv',
+                    index_col = 0,       #None
+                    header = 0, 
+                    sep = ',',
+                    encoding = 'CP949')
 
-hite = pd.read_csv('./data/csv/hite.csv',
-                      index_col = 0,
-                      header = 0,
-                      sep = ',',
-                      encoding = 'cp949')
+# print(samsung)
+# print(hite.head())
+# print(samsung.shape)     #(700 , 1)
+# print(hite.shape)        #(700 , 5)
+
+#Nan 제거1
+
+samsung = samsung.dropna(axis = 0)      # Non이 들어가 있는 행을 삭제하겠다
+# samsung= samsung.dropna(how='all')
 
 print(samsung)
+# print(samsung.shape)     #(  ,   1)
+
+
+hite = hite.fillna(method='bfill')  # bfill= backfill 전날 값으로 채우겠다.
+# hite = hite.dropna(how='all')
+# hite.iloc[0:1:] = hite.iloc[0,1:].fillna(value = str(0))
+# hite1= hite.iloc[1:, :].copy()
+
+
+hite = hite.dropna(axis = 0)           # Nan이 들어간 행을 지우겠다.
+ 
+
 print(hite)
-print(samsung.shape)                # (700, 1)
-print(hite.shape)                   # (720, 5)
 
 
-# None 제거1
-samsung = samsung.dropna(axis = 0)
+#Nan 제거2
+
+# hite = hite[0:509]
+
+# hite.iloc[0,1:5] = ['38750','36000','38750','1407345']   #iloc은 index location 
+
+# hite.loc["2020-06-02", '고가':'거래량'] = ['10','20','30','40']
+print(hite.head)
+
+
+
+#삼성과 하이트의 정렬을 오름차순으로 변경
+samsung = samsung.sort_values(['일자'], ascending = ['True']) #ascending 오름차순
+hite = hite.sort_values(['일자'], ascending = ['True'])       #descending 내림차순
+
+# print(samsung)
+# print(hite)
+
+
+#콤마제거, 문자를 정수로 변환
+for i in range(len(samsung.index)): 
+    samsung.iloc[i,0] = int(samsung.iloc[i,0].replace(',',''))
+
 print(samsung)
-print(samsung.shape)                # (509, 1)
-hite = hite.fillna(method = 'bfill')
-hite = hite.dropna(axis = 0)
-print(hite)
-print(hite.shape)                   # (509, 5)
+  
 
+for i in range(len(hite.index)):
+    for j in range(len(hite.iloc[i])):
+        hite.iloc[i,j] = int(hite.iloc[i,j].replace(',','') )
 
-# None 제거2
-hite = hite[0:509]
-hite.iloc[0, 1:5] = [10, 20, 30, 40]
-hite.loc['2020-06-02, '고가':'거래량'] = ['10', '20', '30', '40']
 print(hite)
+print(type(hite.iloc[1,1]))
+
+print(samsung.shape)      #(509,1)
+print(hite.shape)         #(509,5)
+
+samsung = samsung.values
+hite = hite.values
+
+print(type(hite))         #class 'numpy.ndarray'
+
+np.save('./data/samsung.npy', arr=samsung)
+np.save('./data/hite.npy', arr=hite)
--- test_samsung/test0602_model2.py
+++ test_samsung/test0602_model2.py
@@ -4,7 +4,7 @@ from keras.models import Model, load_model
 from keras.layers import Dense, LSTM, Dropout, Input
 from keras.layers.merge import concatenate, Concatenate
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler,MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.decomposition import PCA
 from keras.models import Sequential, Model
 
@@ -14,52 +14,60 @@ def split_x(seq, size):
     for i in range(len(seq) - size + 1):
         subset = seq[i:(i+size)]
         aaa.append([j for j in subset])
-    #print(type(aaa))
+    # print(type(aaa))
     return np.array(aaa)
 
 size = 6
 
-#1. 데이터
-#npy 불러오기
-samsung = np.load('./data/samsung.npy', allow_pickle='True') #객체 배열(object)를 저장할 수 있게 해줌
-hite = np.load('./data/hite.npy', allow_pickle='True') #object는 str, int와 같은 데이터 타입
+# 1. 데이터
+# npy 불러오기
+samsung = np.load('./data/samsung.npy', allow_pickle = 'True') # 객체 배열(object)를 저장할 수 있게 해줌
+hite = np.load('./data/hite.npy', allow_pickle = 'True') # object는 str, int와 같은 데이터 타입
 
-print(samsung.shape)        #(509, 1)
-print(hite.shape)           #(509, 5)
+print(samsung.shape)        # (509, 1)
+print(hite.shape)           # (509, 5)
 
-samsung = samsung.reshape(samsung.shape[0], )  #(509, )
+samsung = samsung.reshape(samsung.shape[0], )  # (509, )
 
 samsung = (split_x(samsung, size))
-print(samsung.shape)        #(504, 6)
+print(samsung.shape)        # (504, 6)
 
 x_sam = samsung[:, 0:5]
 y_sam = samsung[:, 5]
 
-print(x_sam.shape)          #(504,5)
-print(y_sam.shape)          #(504, )
+print(x_sam.shape)          # (504, 5)
+print(y_sam.shape)          # (504, )
 
 x_hit = hite[5:510, :]
-print(x_hit.shape)          #(504, 5)
+print(x_hit.shape)          # (504, 5)
+
+x_hit = x_hit.reshape(x_hit.shape[0], 5, 1)
+print(x_hit.shape)          # (504, 5, 1)
+
 
 #2. 모델구성 
-
-input1 = Input(shape=(5,))
+input1 = Input(shape = (5, ))
 x1 = Dense(10)(input1)
 x1 = Dense(10)(x1)
 
-input2 = Input(shape=(5,))
-x2 = Dense(5)(input2)
+input2 = Input(shape = (5, 1))
+x2 = LSTM(5)(input2)
 x2 = Dense(5)(x2)
 
 merge = concatenate([x1, x2])
 
 output = Dense(1)(merge)
 
-model = Model(inputs= [input1, input2], output=output)
+model = Model(inputs = [input1, input2], output = output)
 
 model.summary()
 
-# #3. 컴파일, 훈련 
 
-model.compile(loss='mse', optimizer='adam', metrics=['mse'])
-model.fit([x_sam, x_hit], y_sam, epochs=5)    
+# 3. 컴파일, 훈련 
+model.compile(loss = 'mse', optimizer = 'adam', metrics = ['mse'])
+model.fit([x_sam, x_hit], y_sam, epochs = 5)
+
+
+# 4. 모델 평가
+res = model.evaluate(x_sam)
+print(res)
