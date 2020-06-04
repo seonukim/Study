a2777cf7e7e5dd65b5fd57895239643177b0dc95
seonukim <92.seoonooo@gmail.com>
2020-06-04 11:41:32 +0900

    add file

---
+++ ML/m03_xor3.py
@@ -0,0 +1,24 @@
+from sklearn.svm import LinearSVC, SVC
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+
+# 1. 데이터
+x_data = [[0, 0], [1, 0], [0, 1], [1, 1]]
+y_data = [0, 1, 1, 0]
+
+# 2. 모델
+# model = LinearSVC()
+# model = SVC()
+model = KNeighborsClassifier(n_neighbors = 1)
+
+# 3. 훈련
+model.fit(x_data, y_data)
+
+# 4. 평가 예측
+x_test = [[0, 0], [1, 0], [0, 1], [1, 1]]
+y_predict = model.predict(x_test)
+
+acc = accuracy_score([0, 1, 1, 0], y_predict)       # keras의 evaluate와 동일하다고 보면 됨; accuracy_score
+
+print(x_test, "의 예측 결과 : ", y_predict)
+print("acc : ", acc)
---
+++ ML/m4_xor4_keras.py
@@ -0,0 +1,44 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.svm import LinearSVC, SVC
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+
+
+# 1. 데이터
+x_data = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
+y_data = np.array([0, 1, 1, 0])
+
+print(x_data.shape)
+print(y_data.shape)
+
+
+# 2. 모델
+# model = LinearSVC()
+# model = SVC()
+# model = KNeighborsClassifier(n_neighbors = 1)
+model = Sequential()
+
+model.add(Dense(1, input_shape = (2, ) , activation = 'sigmoid'))
+
+model.summary()
+
+
+# 3. 훈련
+model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['acc'])
+model.fit(x_data, y_data, epochs = 100, batch_size = 1)
+
+
+# 4. 평가 예측
+res = model.evaluate(x_data, y_data)
+
+x_test = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
+y_predict = model.predict(x_test)
+
+# acc = accuracy_score([0, 1, 1, 0], y_predict)       # keras의 evaluate와 동일하다고 보면 됨; accuracy_score
+
+# print(x_test, "의 예측 결과 : ", y_predict)
+# print("acc : ", acc)
+
+print("acc : ", res[1])
