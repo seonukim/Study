81fcfb692b91d45358cb41fb19b36b3310c00c92
seonukim <92.seoonooo@gmail.com>
2020-06-05 16:16:40 +0900

    modified

--- ML/m11_gridSearch2.py
+++ ML/m11_gridSearch2.py
@@ -1,39 +1,49 @@
 # k겹 교차 검증
 # GridSearchCV
+# cifar10 적용
 
 import numpy as np
 import pandas as pd
-from sklearn.svm import SVC
+from keras.datasets import mnist
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.model_selection import cross_val_score, GridSearchCV
 from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_breast_cancer
 import warnings
 warnings.filterwarnings('ignore')
 
-# 1. 데이터
-iris = pd.read_csv('./data/csv/iris.csv',
-                   header = 0, sep = ',')
 
-x = iris.iloc[:, :4]
-y = iris.iloc[:, -1:]
-print(x)
-print(y)
+# 1. 데이터
+x, y = load_breast_cancer(return_X_y = True)
+print(x.shape)              # (569, 30)
+print(y.shape)              # (569,)
 
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size = 0.2)
+print(x_train.shape)        # (455, 30)
+print(x_test.shape)         # (114, 30)
+print(y_train.shape)        # (455,)
+print(y_test.shape)         # (114,)
 
-kfold = KFold(n_splits = 5, shuffle = True)
 
-parameters = [
-    {'C' : [1, 10, 100, 1000], 'kernel' : ['linear']},
-    {'C' : [1, 10, 100, 1000], 'kernel' : ['rbf'], 'gamma' : [0.001, 0.0001]},
-    {'C' : [1, 10, 100, 1000], 'kernel' : ['sigmoid'], 'gamma' : [0.001, 0.0001]}
+# 2. KFold_Cross Validation 객체 생성
+kf = KFold(n_splits = 5, shuffle = True)
+
+
+# 3. parameter 정의
+params = [
+    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+    {'n_estimators': [3, 10], 'bootstrap': [False], 'max_features': [2, 4, 6]}
 ]
 
-model = GridSearchCV(SVC(), param_grid = parameters, cv = kfold)
+
+print(params)
+
+model = GridSearchCV(RandomForestClassifier(), param_grid = params, cv = kf, n_jobs = -1)
 
 model.fit(x_train, y_train)
 
 y_pred = model.predict(x_test)
 print("최종 정답률 : ", accuracy_score(y_test, y_pred))
-print("최적의 매개변수 : ", model.best_params_)
+print("parameters : ", model.best_estimator_)
