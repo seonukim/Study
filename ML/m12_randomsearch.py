18a5d01063a8c7ab31611a899d9cb64c1d6cc409
seonukim <92.seoonooo@gmail.com>
2020-06-05 17:43:12 +0900

    add file

--- ML/m12_randomsearch.py
+++ ML/m12_randomsearch.py
@@ -1,6 +1,5 @@
-# k겹 교차 검증
-# GridSearchCV
-# cifar10 적용
+# cancer 적용
+# RandomizedSearchCV
 
 import numpy as np
 import pandas as pd
@@ -8,6 +7,7 @@ from keras.datasets import mnist
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import accuracy_score
 from sklearn.datasets import load_breast_cancer
 import warnings
@@ -32,23 +32,21 @@ kf = KFold(n_splits = 5, shuffle = True)
 
 
 # 3. parameter 정의
-params = [
-    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
-    {'n_estimators': [3, 10], 'bootstrap': [False], 'max_features': [2, 4, 6]}
-]
+params = {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8], 
+          'n_estimators': [3, 10], 'bootstrap': [False], 'max_features': [2, 4, 6]}
 
 
 print(params)
 
-model = GridSearchCV(RandomForestClassifier(), param_grid = params, cv = kf, n_jobs = -1)
+model = RandomizedSearchCV(RandomForestClassifier(), param_distributions = params, cv = kf, n_jobs = -1)
 
 model.fit(x_train, y_train)
 
 y_pred = model.predict(x_test)
-print("최종 정답률 : ", accuracy_score(y_test, y_pred))
+print("최종 정답률 : ", accuracy_score(y_test, y_pred))     # 0.9473684210526315
 print("parameters : ", model.best_estimator_)
 print("best_params : ", model.best_params_)
 
 '''
-best_params :  {'max_features': 4, 'n_estimators': 30}
+best_params :  {'n_estimators': 3, 'max_features': 6, 'bootstrap': False}
 '''
