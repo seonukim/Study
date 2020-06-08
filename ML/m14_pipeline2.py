8e6607e82e097477df8fbe40889b3ee712cbc77e
seonukim <92.seoonooo@gmail.com>
2020-06-08 16:05:18 +0900

    add file

--- ML/m14_pipeline2.py
+++ ML/m14_pipeline2.py
@@ -4,7 +4,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.svm import SVC
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 # 1. 데이터
@@ -16,10 +16,22 @@ x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size = 0.2,
     shuffle = True, random_state = 77)
 
-# 2. 모델
-model = SVC()
-pipe = Pipeline([('scaler', MinMaxScaler()), ('svm', SVC())])
+# 그리드/랜덤 서치에서 사용할 매개변수
+param = [
+    {"svm__C": [1, 10, 100, 1000], "svm__kernel": ['linear']},
+    {"svm__C": [1, 10, 100, 1000], "svm__kernel": ['rbf'], "svm__gamma": [0.001, 0.0001]},
+    {"svm__C": [1, 10, 100, 1000], "svm__kernel": ['sigmoid'], "svm__gamma": [0.001, 0.0001]}
+]
 
-pipe.fit(x_train, y_train)
-print('acc : ', pipe.score(x_test, y_test))
-# acc :  0.8666666666666667
+# 2. 모델
+pipe = Pipeline([('scaler', MinMaxScaler()), ("svm", SVC())])
+# pipe = make_pipeline(MinMaxScaler(), SVC())
+
+model = RandomizedSearchCV(pipe, param_distributions = param, cv = 5)
+
+# 3. 평가 및 예측
+model.fit(x_train, y_train)
+
+acc = model.score(x_test, y_test)
+print("최적의 매개 변수 : ", model.best_params_)
+print("Accuracy : ", acc)
