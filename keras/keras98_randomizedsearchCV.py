b043fcac58eff14f329afa0c837c7827c362543b
seonukim <92.seoonooo@gmail.com>
2020-06-08 11:46:08 +0900

    add .score

--- keras/keras98_randomizedsearchCV.py
+++ keras/keras98_randomizedsearchCV.py
@@ -58,9 +58,11 @@ model = KerasClassifier(build_fn = build_model, verbose = 1)
 # hyperparameters 변수 정의
 hyperparameters = create_hyperparameter()
 
-search = RandomizedSearchCV(estimator = model, n_jobs = 1,
+search = RandomizedSearchCV(estimator = model, 
                             param_distributions = hyperparameters, cv = 3)
 
 # 모델 훈련
 search.fit(x_train, y_train)
-print(search.best_params_)
+score = search.score(x_test, y_test)
+print(search.best_params_)              # {'optimizer': 'adam', 'drop': 0.1, 'batch_size': 10}
+print("score : ", score)
