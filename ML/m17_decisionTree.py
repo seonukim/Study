b825d0f39207169835d2e046db6a366c66ea5b02
seonukim <92.seoonooo@gmail.com>
2020-06-10 16:32:26 +0900

    add file

--- ML/m17_decisionTree.py
+++ ML/m17_decisionTree.py
@@ -4,10 +4,10 @@ from sklearn.model_selection import train_test_split
 
 cancer = load_breast_cancer()
 x_train, x_test, y_train, y_test = train_test_split(
-    cancer.data, cancer.target, train_size =0.8, random_state= 42
+    cancer.data, cancer.target, train_size  = 0.8, random_state = 42
 )
 
-model = DecisionTreeClassifier(max_depth = 4 )
+model = DecisionTreeClassifier(max_depth = 4)
 
 model.fit(x_train, y_train)
 
