76daeb0428965bfe97ade3adbeb76e3ca8d0b4a7
seonukim <92.seoonooo@gmail.com>
2020-06-22 09:39:19 +0900

    create eval_graph

---
+++ ML/m28_eval_graph.py
@@ -0,0 +1,36 @@
+# xgboost evaluate
+
+import numpy as np
+from sklearn.feature_selection import SelectFromModel
+from xgboost import XGBRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from sklearn.datasets import load_boston
+
+## 데이터
+x, y = load_boston(return_X_y = True)
+print(x.shape)          # (506, 13)
+print(y.shape)          # (506,)
+
+## train_test_split
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size = 0.2,
+    shuffle = True, random_state = 66)
+
+## 모델링
+model = XGBRegressor(n_estimators = 3,        # verbose의 갯수, epochs와 동일
+                     learning_rate = 0.1)
+
+model.fit(x_train, y_train,
+          verbose = True, eval_metric = ['logloss', 'rmse'],
+          eval_set = [(x_train, y_train),
+                      (x_test, y_test)],
+          early_stopping_rounds = 20)
+# eval_metic의 종류 : rmse, mae, logloss, error(error가 0.2면 accuracy는 0.8), auc(정확도, 정밀도; accuracy의 친구다)
+
+results = model.evals_result()
+print("eval's result : ", results)
+
+y_pred = model.predict(x_test)
+r2 = r2_score(y_test, y_pred)
+print("R2 : ", r2)
