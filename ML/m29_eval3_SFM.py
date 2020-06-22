5ca3c32272e6f88bdd9ce3e3738201dea6f15508
seonukim <92.seoonooo@gmail.com>
2020-06-22 12:22:01 +0900

    create new file

--- ML/m29_eval3_SFM.py
+++ ML/m29_eval3_SFM.py
@@ -7,22 +7,22 @@ from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectFromModel
-from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import load_iris
 warnings.filterwarnings('ignore')
 
 ## 데이터
-x, y = load_breast_cancer(return_X_y = True)
-print(x.shape)          # (569, 30)
-print(y.shape)          # (569,)
+x, y = load_iris(return_X_y = True)
+print(x.shape)          # (150, 4)
+print(y.shape)          # (150,)
 
 ## train_test_split
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size = 0.2,
     shuffle = True, random_state = 66)
-print(x_train.shape)        # (455, 30)
-print(x_test.shape)         # (114, 30)
-print(y_train.shape)        # (455,)
-print(y_test.shape)         # (114,)
+print(x_train.shape)        # (120, 4)
+print(x_test.shape)         # (30, 4)
+print(y_train.shape)        # (120,)
+print(y_test.shape)         # (30,)
 
 ## 모델링
 model = XGBClassifier(n_estimators = 1000,
@@ -31,7 +31,7 @@ model = XGBClassifier(n_estimators = 1000,
 
 model.fit(x_train, y_train,
           verbose = True,
-          eval_metric = ['logloss', 'error'],
+          eval_metric = ['mlogloss', 'merror'],
           eval_set = [(x_train, y_train),
                       (x_test, y_test)],
           early_stopping_rounds = 100)
@@ -52,9 +52,10 @@ for i in thresholds:
     select_model = XGBClassifier(n_estimators = 1000,
                                  learning_rate = 0.01,
                                  n_jobs = 6)
+
     select_model.fit(select_x_train, y_train,
                      verbose = True,
-                     eval_metric = ['logloss', 'error'],
+                     eval_metric = ['mlogloss', 'merror'],
                      eval_set = [(select_x_train, y_train),
                                  (select_x_test, y_test)],
                      early_stopping_rounds = 100)
@@ -64,4 +65,4 @@ for i in thresholds:
     print("Thresh = %.3f, n = %d, acc : %.2f%%"
           %(i, select_x_train.shape[1], score * 100.0))
 
-# acc : 0.9649122807017544
+# Thresh = 0.005, n = 4, acc : 100.00%
