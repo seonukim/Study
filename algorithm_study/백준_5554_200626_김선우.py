1f0f09e52b40bfbd4354b563140df22f91a2fd3d
seonukim <92.seoonooo@gmail.com>
2020-06-26 17:11:51 +0900

    백준 문제 풀이

---
+++ algorithm_study/백준_5554_200626_김선우.py
@@ -0,0 +1,5 @@
+home_to_school = int(input())
+school_to_pc = int(input())
+pc_to_academy = int(input())
+academy_to_home = int(input())
+total = home_to_school + school_to_pc + pc_to_academy + academy_to_home
