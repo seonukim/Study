d462d0e8bc87889451e17e2790b7f88e9ef3d55d
seonukim <92.seoonooo@gmail.com>
2020-06-26 16:58:30 +0900

    백준 문제 풀이

--- algorithm_study/백준_5522_200626_김선우.py
+++ algorithm_study/백준_5522_200626_김선우.py
@@ -1,18 +1,7 @@
-# a = 0
-# for i in range(5):
-    
-
-x,y,z = 0,0,0
-chk=False
-for x in range(1,7):
-    for y in range(1,7):
-        for z in range(1,7):
-            if x !=y and y!=z and z!=x:
-                if x*y*z == x+y+z+3:
-                    print(x,y,z)
-                    chk=True
-                    break
-        if chk:
-            break
-    if chk:
-        break
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = a + b + c + d + e
+print(f)
