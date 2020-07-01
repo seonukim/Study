2e91b478d1fe2f4c03e9492674ed2c6823d72d47
seonukim <92.seoonooo@gmail.com>
2020-07-01 10:57:45 +0900

    백준 문제 풀이

--- algorithm_study/백준_5554_200626_김선우.py
+++ algorithm_study/백준_5554_200626_김선우.py
@@ -1,5 +1,12 @@
-home_to_school = int(input())
-school_to_pc = int(input())
-pc_to_academy = int(input())
-academy_to_home = int(input())
-total = home_to_school + school_to_pc + pc_to_academy + academy_to_home
+import sys
+input = sys.stdin.readline
+
+h_to_s = int(input())
+s_to_p = int(input())
+p_to_a = int(input())
+a_to_h = int(input())
+
+tot_min = (h_to_s + s_to_p + p_to_a + a_to_h) // 60
+tot_sec = (h_to_s + s_to_p + p_to_a + a_to_h) % 60
+print(tot_min)
+print(tot_sec)
