fe0cf627762fb73406061128fcc57d2c46044d22
seonukim <92.seoonooo@gmail.com>
2020-07-02 13:59:14 +0900

    RandomizedSearch에 lr적용

--- keras/keras107_RS_lr.py
+++ keras/keras107_RS_lr.py
@@ -8,9 +8,10 @@ from keras.models import Sequential, Model
 from keras.layers import Dense, Input, LSTM
 from keras.layers import Conv2D, Flatten
 from keras.layers import MaxPooling2D, Dropout
+from keras.optimizers import Adam, Adadelta, Adagrad
+from keras.optimizers import RMSprop, Nadam, SGD, Adamax
 from keras.wrappers.scikit_learn import KerasClassifier     # 케라스를 사이킷런으로 감싼다. (사이킷런에서 쓸 수 있게)
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-
 
 # 1. 데이터
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -19,12 +20,12 @@ print(x_test.shape)                 # (10000, 28, 28)
 print(y_train.shape)                # (60000,)
 print(y_test.shape)                 # (10000,)
 
-# x_train = x_train.reshape(x_train.shape[0], 28, 28, 1) / 255          # 정규화(min_max)
-# x_test = x_test.reshape(x_test.shape[0], 28, 28, 1) / 255             # 정규화(min_max)
-# x_train = x_train.reshape(x_train.shape[0], 28 * 28) / 255            # 정규화(min_max)
-# x_test = x_test.reshape(x_test.shape[0], 28 * 28) / 255               # 정규화(min_max)
-# print(x_train.shape)                # (60000, 28, 28, 1)
-# print(x_test.shape)                 # (10000, 28, 28, 1)
+x_train = x_train.reshape(x_train.shape[0], 28*28) / 255            # 정규화(min_max)
+x_test = x_test.reshape(x_test.shape[0], 28*28) / 255               # 정규화(min_max)
+x_train = x_train.reshape(x_train.shape[0], 28*28) / 255            # 정규화(min_max)
+x_test = x_test.reshape(x_test.shape[0], 28*28) / 255               # 정규화(min_max)
+print(x_train.shape)                # (60000, 784)
+print(x_test.shape)                 # (10000, 784)
 
 y_train = np_utils.to_categorical(y_train)      # label이 0부터 시작함
 y_test = np_utils.to_categorical(y_test)        # label이 0부터 시작함
@@ -33,27 +34,38 @@ print(y_test.shape)                 # (10000, 10)
 
 
 # 2. 모델링
-def build_model(drop = 0.5, optimizer = 'adam'):
-    inputs = Input(shape = (28, 28), name = 'input')
-    x = LSTM(64, activation = 'relu', return_sequences = True, name = 'hidden1')(inputs)
+def build_model(drop, optimizer, lr): 
+    inputs = Input(shape = (784,), name = 'input')
+    x = Dense(64, activation = 'relu', name = 'hidden1')(inputs)
     x = Dropout(drop)(x)
-    x = LSTM(32, activation = 'relu', name = 'hidden2')(x)
+    x = Dense(32, activation = 'relu', name = 'hidden2')(x)
     x = Dropout(drop)(x)
     x = Dense(16, activation = 'relu', name = 'hidden3')(x)
     x = Dropout(drop)(x)
     outputs = Dense(10, activation = 'softmax', name = 'output')(x)
     model = Model(inputs = inputs, outputs = outputs)
-    model.compile(optimizer = optimizer, metrics = ['accuracy'],
+    model.compile(optimizer = optimizer(lr = lr), metrics = ['accuracy'],
                   loss = 'categorical_crossentropy')
     return model
 
+## 최적화 함수 정의
+# adam = Adam(lr = lr)
+# adadelta = Adadelta(lr = lr)
+# adamax = Adamax(lr = lr)
+# nadam = Nadam(lr = lr)
+# rmsprop = RMSprop(lr = lr)
+# adagrad = Adagrad(lr = lr)
+# sgd = SGD(lr = lr)
+
 def create_hyperparameter():
     batches = [10, 20, 30, 40, 50]
-    optimizers = ['rmsprop', 'adam', 'adadelta']
+    optimizers = [Adam, Adadelta, Adamax, Nadam, RMSprop, Adagrad, SGD]
     dropout = np.linspace(0.1, 0.5, 5)
+    lr = np.linspace(0.001, 0.01, 10).tolist()
     return {'batch_size': batches,
             'optimizer': optimizers,
-            'drop': dropout}
+            'drop': dropout,
+            'lr': lr}
 
 # KerasClassifier 모델 구성하기
 model = KerasClassifier(build_fn = build_model, verbose = 1)
