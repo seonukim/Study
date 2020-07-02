eac39b0d78a6489be93507e840e40f62e0494b34
seonukim <92.seoonooo@gmail.com>
2020-07-02 16:53:04 +0900

    activation

--- keras/keras110_4_ELU.py
+++ keras/keras110_4_ELU.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 x = np.arange(-5, 5, 0.1)
 
 def elu_func(x):    # ELU(Exponential Linear Unit)
-    return (x >= 0)*x + (x < 0)*0.01*(np.exp(x)-1)
+    return (x>=0)*x + (x<0)*0.01*(np.exp(x)-1)
 
 plt.plot(x, elu_func(x), linestyle = '--', label = 'ELU')
 plt.show()
---
+++ keras/keras110_6_trelu.py
@@ -0,0 +1,13 @@
+# activation - TReLU
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-6, 6, 0.01)
+
+def trelu_func(x):
+    return (x > 1)*x    # 임계값 (1) 조정 가능
+
+plt.plot(x, trelu_func(x), linestyle = '--', label = "Threshholded ReLU")
+plt.ylim(-5, 5)
+plt.show()
