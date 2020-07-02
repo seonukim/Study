1ddd06d8f4c6e4c360041196fef158300ec5dc4b
seonukim <92.seoonooo@gmail.com>
2020-07-02 17:13:43 +0900

    activation

--- keras/keras110_4_ELU.py
+++ keras/keras110_4_ELU.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 
 x = np.arange(-5, 5, 0.1)
 
-def elu_func(x):    # ELU(Exponential Linear Unit)
-    return (x>=0)*x + (x<0)*0.01*(np.exp(x)-1)
+def elu(x):
+    x = np.copy(x)
+    x[x < 0] = 0.2 * (np.exp(x[x < 0]) - 1)
+    return x
 
-plt.plot(x, elu_func(x), linestyle = '--', label = 'ELU')
+plt.plot(x, elu(x), linestyle = '--', label = 'ELU')
 plt.show()
--- keras/keras110_5_LeakyReLU.py
+++ keras/keras110_5_LeakyReLU.py
@@ -5,9 +5,8 @@ import matplotlib.pyplot as plt
 
 x = np.arange(-6, 6, 0.01)
 
-def leakyrelu_func(x):      # Leaky ReLU(Rectified Linear Unit)
-    return (x>=0)*x + (x<0)*0.01*x  # 알파값(보통 0.01) 조정 가능
-    # return np.maximum(0.01*x, x)  #same
+def leakyrelu(x):      # Leaky ReLU(Rectified Linear Unit)
+    return np.maximum(0.01 * x, x)  #same
 
-plt.plot(x, leakyrelu_func(x), linestyle = '--', label = 'Leaky ReLU')
+plt.plot(x, leakyrelu(x), linestyle = '--', label = 'Leaky ReLU')
 plt.show()
