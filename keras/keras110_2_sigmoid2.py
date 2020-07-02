43f0150433bb02a946f7c0310a36df5996ebaad2
seonukim <92.seoonooo@gmail.com>
2020-07-02 16:10:35 +0900

    활성화함수 시그모이드 아웃풋 레이어에만?

--- keras/keras110_2_sigmoid2.py
+++ keras/keras110_2_sigmoid2.py
@@ -1,35 +1,37 @@
+# 활성화 함수 - sigmoid
+# activation의 목적 - 가중치 값을 한정시킨다.
+
 # 1. 데이터
 import numpy as np
 x_train = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-y1_train = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-y2_train = np.array([1, 0, 1, 0, 1, 0, 1, 0, 1, 0])
+y_train = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 # 2. 모델 구성
 from keras.models import Sequential, Model
 from keras.layers import Dense, Input
-input1 = Input(shape = (1,))
-x1 = Dense(100)(input1)
-x1 = Dense(100)(x1)
-x1 = Dense(100)(x1)
 
-x2 = Dense(50)(x1)
-output1 = Dense(1)(x2)
+model = Sequential()
+model.add(Dense(100, input_shape = (1,)))
+model.add(Dense(100))
+model.add(Dense(100))
+model.add(Dense(100))
+model.add(Dense(1, activation = 'sigmoid'))
 
-x3 = Dense(70)(x1)
-x3 = Dense(70)(x3)
-output2 = Dense(1, activation = 'sigmoid')(x3)
-
-model = Model(inputs = input1, outputs = [output1, output2])
 model.summary()
 
+
 # 3. 컴파일 훈련
-model.compile(loss = ['mse', 'binary_crossentropy'], optimizer = 'adam',
-              metrics = ['mse', 'acc'])
-model.fit(x_train, [y1_train, y2_train], epochs = 100, batch_size = 1)
+model.compile(loss = ['binary_crossentropy'],
+              optimizer = 'adam',
+              metrics = ['acc'])
+
+model.fit(x_train, y_train, epochs = 100, batch_size = 1)
+
 
 # 4. 평가 예측
-loss = model.evaluate(x_train, [y1_train, y2_train])
+loss = model.evaluate(x_train, y_train)
+print('loss : ', loss)
 
-x1_pred = model.predict([11, 12, 14, 15])
+x1_pred = np.array([11, 12, 13, 14])
 y_pred = model.predict(x1_pred)
 print(y_pred)
