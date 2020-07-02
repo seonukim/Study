980d113eb9ee6b921956a5d45a20906ce4c0abc0
seonukim <92.seoonooo@gmail.com>
2020-07-02 17:35:23 +0900

    trelu

--- keras/keras110_6_trelu.py
+++ keras/keras110_6_trelu.py
@@ -6,8 +6,7 @@ import matplotlib.pyplot as plt
 x = np.arange(-6, 6, 0.01)
 
 def trelu_func(x):
-    return (x > 1)*x    # 임계값 (1) 조정 가능
+    return (x > 1) * x    # 임계값 (1) 조정 가능
 
 plt.plot(x, trelu_func(x), linestyle = '--', label = "Threshholded ReLU")
-plt.ylim(-5, 5)
 plt.show()
