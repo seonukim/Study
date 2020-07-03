86b32edcdcb3d4846c481720a411a7ad2f808dcc
seonukim <92.seoonooo@gmail.com>
2020-07-03 11:11:23 +0900

    dropout graph

--- keras/keras114_dropout.py
+++ keras/keras114_dropout.py
@@ -48,7 +48,7 @@ model = Sequential()
 model.add(Conv2D(filters = 32, kernel_size = (3, 3),
                  input_shape = (32, 32, 3), padding = 'same',
                  activation = 'relu'))
-model.add(Dropout(rate = 0.2))
+# model.add(Dropout(rate = 0.2))
 model.add(Conv2D(filters = 32, kernel_size = (3, 3), 
                  padding = 'same', activation = 'relu'))
 model.add(MaxPooling2D(pool_size = (2, 2), strides = 2, padding = 'same'))
@@ -56,7 +56,7 @@ model.add(Dropout(rate = 0.2))
 
 model.add(Conv2D(filters = 64, kernel_size = (3, 3),
                  padding = 'same', activation = 'relu'))
-model.add(Dropout(rate = 0.2))
+# model.add(Dropout(rate = 0.2))
 model.add(Conv2D(filters = 64, kernel_size = (3, 3),
                  padding = 'same', activation = 'relu'))
 model.add(MaxPooling2D(pool_size = (2, 2), strides = 2, padding = 'same'))
@@ -64,7 +64,7 @@ model.add(Dropout(rate = 0.2))
 
 model.add(Conv2D(filters = 128, kernel_size = (3, 3),
                  padding = 'same', activation = 'relu'))
-model.add(Dropout(rate = 0.2))
+# model.add(Dropout(rate = 0.2))
 model.add(Conv2D(filters = 128, kernel_size = (3, 3),
                  padding = 'same', activation = 'relu'))               # hidden layer의 넣고 싶은 곳에 넣어준다.
 model.add(MaxPooling2D(pool_size = (2, 2), strides = 2, padding = 'same'))
