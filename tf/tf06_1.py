cd9b8ee3aaceba3137f9bbd291fcc2f9dc4befe2
seonukim <92.seoonooo@gmail.com>
2020-07-08 10:37:25 +0900

    수정

--- tf/tf06_1.py
+++ tf/tf06_1.py
@@ -2,7 +2,7 @@
 
 import warnings ; warnings.filterwarnings('ignore')
 import tensorflow as tf
-tf.set_random_seed(777)
+tf.compat.v1.set_random_seed(777)
 
 # x_train = [1, 2, 3]
 # y_train = [3, 5, 7]
@@ -12,8 +12,8 @@ y_train = tf.compat.v1.placeholder(tf.float32, shape = [None])
 
 fedict = {x_train: [1, 2, 3], y_train: [3, 5, 7]}
 
-W = tf.Variable(tf.random_normal([1]), name = 'weight')     # 랜덤 정규분포
-b = tf.Variable(tf.random_normal([1], name = 'bias'))       # [] 안의 숫자는 shape를 의미한다
+W = tf.Variable(tf.random.normal([1]), name = 'weight')     # 랜덤 정규분포
+b = tf.Variable(tf.random.normal([1], name = 'bias'))       # [] 안의 숫자는 shape를 의미한다
 
 # sess = tf.Session()
 # sess.run(tf.global_variables_initializer())     # 초기화 ; tf는 초기화를 시켜주어야 한다
@@ -23,11 +23,12 @@ hypothesis = x_train * W + b
 
 cost = tf.reduce_mean(tf.square(hypothesis - y_train))      # 비용함수(loss) 정의 ; 이건 mse이다
 
-train = tf.train.GradientDescentOptimizer(learning_rate = 0.01).minimize(cost)
+train = tf.compat.v1.train.GradientDescentOptimizer(learning_rate = 0.01).minimize(cost)
 # GradientDescentOptimizer = 경사하강 최적화    minimize(cost) ; cost를 최소화하는 지점을 찾아라
+init = tf.compat.v1.global_variables_initializer()
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())         # with문 안에 나오는 모든 변수들을 초기화
+with tf.compat.v1.Session() as sess:
+    sess.run(init)         # with문 안에 나오는 모든 변수들을 초기화
 
     for step in range(2001):
         _, cost_val, W_val, b_val = sess.run([train, cost, W, b], feed_dict = fedict)       # keras의 compile ; optimizer 부분이다
