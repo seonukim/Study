f6eb38039cffcf464af36a6a75f6c2d2db8ac623
seonukim <92.seoonooo@gmail.com>
2020-07-13 18:08:53 +0900

    tf21_rnn2

--- tf/tf21_rnn2.py
+++ tf/tf21_rnn2.py
@@ -1,65 +1,63 @@
-import numpy as np
 import tensorflow as tf
+import numpy as np
+from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score
 
-dataset = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-print(dataset.shape)        # (10,)
+dataset = np.array([1,2,3,4,5,6,7,8,9,10])
+print(dataset.shape) # (10,)
 
-# # RNN 모델을 짜시오!
+# RNN 모델을 짜시오
+size = 5 
+
 def split_x(seq, size):
-    aaa = []
-    for i in range(len(dataset) - size + 1):
-        subset = dataset[i : (i + size)]
+    aaa = [] # 임심 메모리 리스트
+    for i in range(len(seq) - size + 1):#seq - size +1  = 값 행종료값
+        subset = seq[i: (i+size)] #열 지정
         aaa.append([item for item in subset])
     print(type(aaa))
     return np.array(aaa)
 
-# 데이터 자르기
-data = split_x(dataset, 5)
-print(data.shape)           # (6, 5)
-print(data)
+dataset = split_x(dataset, size)
+print(dataset)
 
-x_data = data[:, :4]
-y_data = data[:, 4:]
-print(x_data.shape)         # (6, 4)
-print(y_data.shape)         # (6, 1)
+x_data = dataset[:, 0:4]
+print(x_data.shape) #(6,4)
+print(x_data)
+y_data = dataset[:, 4]
+print(y_data.shape) #(6,)
+print(y_data)
 
 x_data = x_data.reshape(1, 6, 4)
-y_data = y_data.reshape(1, 6)
-print(x_data.shape)         # (1, 6, 4)
-print(y_data.shape)         # (1, 6)
+y_data = y_data.reshape(6, 1)
 
-## 모델 구성
 sequence_length = 6
 input_dim = 4
-output = 100
+output = 4
 batch_size = 1
-EPOCH = 400
 
-X = tf.compat.v1.placeholder(tf.float32, shape = (None, sequence_length, input_dim))
-Y = tf.compat.v1.placeholder(tf.int64, shape = (None, sequence_length))
-print(X)                    # Tensor("Placeholder:0", shape=(?, 6, 4), dtype=float32)
-print(Y)                    # Tensor("Placeholder_1:0", shape=(?, 6), dtype=int64)
+X = tf.compat.v1.placeholder(tf.float32, (None, sequence_length, input_dim))
+Y = tf.compat.v1.placeholder(tf.float32, (None, 1))
 
-_lstm = tf.keras.layers.LSTMCell(output)
-hypothesis, _states = tf.nn.dynamic_rnn(_lstm, X, dtype = tf.float32)
-print(hypothesis)           # Tensor("rnn/transpose_1:0", shape=(?, 6, 1), dtype=float32)
+#2. 모델 생성
+cell = tf.keras.layers.LSTMCell(output)
+hypothesis, _states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
+print(hypothesis.shape) #(?, 6, 4)
 
-## 컴파일
-weight = tf.compat.v1.ones([batch_size, sequence_length])
-sequence_loss = tf.contrib.seq2seq.sequence_loss(
-    logits = hypothesis, targets = Y, weights = weight)
-cost = tf.compat.v1.reduce_mean(sequence_loss)
-train = tf.compat.v1.train.AdamOptimizer(learning_rate = 0.01).minimize(cost)
+w = tf.Variable(tf.random_normal([4, 1]), name = 'weights')
+b = tf.Variable(tf.random_normal([1]), name = 'bias') 
+hypothesis = tf.matmul(X, w) + b     
 
-prediction = tf.math.argmax(hypothesis, axis = 2)
-print(f'Prediction : {prediction}')
+#3.컴파일
+# weights = tf.ones([batch_size, sequence_length])
+cost =  tf.reduce_mean(tf.square( hypothesis - Y))
 
-# 3-2. 훈련
-with tf.compat.v1.Session() as sess:
-    sess.run(tf.compat.v1.global_variables_initializer())
+train = tf.compat.v1.train.AdamOptimizer(learning_rate = 0.001).minimize(cost)
 
-    for i in range(401):
-        loss, _ = sess.run([cost, train], feed_dict = {X: x_data, Y: y_data})
-        result = sess.run(prediction, feed_dict = {X: x_data})
-        print(f'\nEpoch : {i}, Prediction : {result}, true Y : {y_data}')
-        print(f'\nLoss : {loss}')
+#3-2. 훈련
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(1500):
+        loss, _ = sess.run([cost, train], feed_dict = {X: x_data, Y: y_data}) #볼필요가 없으면 _만 사용한다.
+        print(i,"loss :",loss)
+    y_pred =sess.run(hypothesis, feed_dict = {X: x_data})
+    print(y_pred)
