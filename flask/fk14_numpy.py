f387d9e7c9a49128e36880602aa9faf57d1a6857
seonukim <92.seoonooo@gmail.com>
2020-07-14 15:35:25 +0900

    커밋

---
+++ flask/fk14_numpy.py
@@ -0,0 +1,18 @@
+import pymssql as ms
+import numpy as np
+
+conn = ms.connect(server = '127.0.0.1', user = 'bit2',
+                  password = '1234', database = 'bitdb')
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM iris2;')
+row = cursor.fetchall()
+conn.close()
+aaa = np.asarray(row)
+np.save('./data/test_flask_iris2.npy', arr = aaa)
+
+
+print(row)
+print("=" * 40)
+print(aaa)
+print(aaa.shape)
+print(type(aaa))
