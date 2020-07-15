86036bc27af9dcb3911f694560e00f12cc1256d7
seonukim <92.seoonooo@gmail.com>
2020-07-15 10:17:14 +0900

    fk

--- flask/fk17_board.py
+++ flask/fk17_board.py
@@ -17,9 +17,34 @@ def run():
     c = conn.cursor()
     c.execute("SELECT * FROM general")
     rows = c.fetchall();
-    return render_template("./template/board_index.html", rows = rows)
+    return render_template("board_index.html", rows = rows)
 
 @app.route('/modi')
+def modi():
+    id = request.args.get('id')
+    conn = sqlite3.connect('./data/wanggun.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM general WHERE id = ' + str(id))
+    rows = c.fetchall();
+    return render_template('board_modi.html', rows = rows)
 
+@app.route('/addrec', methods=['POST', 'GET'])
+def addrec():
+    if request.methods == 'POST':
+        try:                    # 예외 처리
+            war = request.form['war']
+            id = request.form['id']
+            with sqlite3.connect("./data/wanggun.db") as conn:
+                cur = conn.cursor()
+                cur.execute("UPDATE general SET war=" + str(war) + "WHERE id=" + str(id))
+                conn.commit()
+                msg = '정상적으로 입력되었습니다.'
+        except:
+            conn.rollback()                 # 에러 뜨면 되돌리기
+            msg = '입력 과정에서 에러가 발생했습니다.'
+        
+        finally:
+            return render_template('board_result.html', msg = msg)
+            conn.close()
 
-app.run(host = '127.0.0.1', port = 5001, debug = False)
+app.run(host='127.0.0.1', port=5001, debug=False)
