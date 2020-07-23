3a731d0e064b900ea8fb8d18bae7d1ec03cf3727
seonukim <92.seoonooo@gmail.com>
2020-07-23 09:17:52 +0900

    commit

--- 1.Project/(1)_code/class_project.py
+++ 1.Project/(1)_code/class_project.py
@@ -1,44 +1,26 @@
 import numpy as np
 import cv2, os, dlib
 
-class Project:                                  # 해당 경로의 파일들 처리
-    def __init__(self, path):
-        self.top_path = path
-        self.category = os.listdir(top_path)    # list로 반환
+class Project:                                              # 해당 경로의 파일들 처리
+    def __init__(self):
         pass
 
-    # 이미지 불러오기
-    def load_image(self):
-        img_b = []
-        for folder in (self.category):                      # folder 이름
-            self.image_dir = self.top_path + '/'+folder+'/' # 찾을 image 경로
+    def face_detector(self, img, detector_path):
 
-            for top, dir, f in os.walk(image_dir):          # image 이름 탐색
-                for filename in f:
-                    img = cv2.imread(self.image_dir+filename)
-                    self.img_b.append(img)
-
-        return self.img_b
-
-    def face_detector(self, detector_path):
-        img = cv2.cvtColor(self.img_b, cv2.COLOR_BGR2RGB)
-
-        self.img_copy = img.copy()
+        self.img_face = img.copy()
 
         detector = dlib.cnn_face_detection_model_v1(detector_path)
-        self.dets = detector(img, upsample_num_times=1)
+        self.dets = detector(self.img_face, upsample_num_times=1)
         
         return self.dets
 
-    def bbox(self):
-        bbox = []
-
-        d = self.dets
-        print("Detection {}: Left:{} Top:{} Right:{} Bottom:{} Confidence:{}".format(i+1, 
-                d.rect.left(), d.rect.top(), d.rect.right(), d.rect.bottom(), d.confidence))
+    def BBox(self, number, dets):
+        d = dets
+        # print("Detection {}: Left:{} Top:{} Right:{} Bottom:{} Confidence:{}".format(number+1, 
+        #         d.rect.left(), d.rect.top(), d.rect.right(), d.rect.bottom(), d.confidence))
 
         x1, y1 = d.rect.left(), d.rect.top()
-        X2, Y2 = d.rect.right(), d.rect.bottom()
+        x2, y2 = d.rect.right(), d.rect.bottom()
 
         #-------- bbox 키우기-----------
         pad = (x2 - x1)
@@ -49,44 +31,36 @@ class Project:                                  # 해당 경로의 파일들 처
 
         if x1 < 0:
             x1 = 0
-        elif y1 < 0:
+        if y1 < 0:
             y1 = 0
 
         x1, x2, y1, y2 = map(int, (x1, x2, y1, y2))     # int형으로 변환
-        self.bbox = bbox.append([x1, x2, y1, y2])
-
-        img_bbox = self.img_copy
-        cv2.rectangle(img_bbox, (x1, y1), (x2, y2), thickness=1, color=(255, 122, 122), lineType=cv2.LINE_AA)
+        self.bbox = [x1, x2, y1, y2]
             
-        self.img_bbox = img_bbox                            # image
-        cv2.imshow('BBox',self.img_bbox)
-        cv2.waitKey(0)
+        img_bbox = self.img_face.copy()
+        cv2.rectangle(img_bbox, (x1, y1), (x2, y2), thickness=1, color=(255, 122, 122), lineType=cv2.LINE_AA)
+                
+        self.img_bbox = img_bbox                        # image
+        # cv2.imshow('BBox',self.img_bbox)
+        # cv2.waitKey(0)
+        # cv2.destroyWindow('BBox')
         
-        return  self.bbox
+        return self.bbox        # 수정한 bounding box 좌표
 
     def crop(self):
-        x1 = self.bbox[0]
-        x2 = self.bbos[1]
-        y1 = self.bbox[2]
-        y2 = self.bbox[3]
+        x1 = self.bbox[0]       # x1
+        x2 = self.bbox[1]       # x2
+        y1 = self.bbox[2]       # y1    
+        y2 = self.bbox[3]       # y2
 
-        self.img_crop = self.img_copy[y1:y2, x1:x2]
-        cv2.imshow('Crop', self.img_crop)
-        cv2.waitKey(0)
+        self.img_crop = self.img_face[y1:y2, x1:x2]
+        # cv2.imshow('Crop', self.img_crop)
+        # cv2.waitKey(0)
+        # cv2.destroyWindow('Crop')
 
-        return self.img_crop
+        return self.img_crop    # 자른 이미지
 
-    def labeling(self):
-        num_class = len(self.category)
-        labeling = []
 
-        for idex, folder in enumerate(self.category):
-            label = [0 for i in range(num_class)]
-            label[idex] = 1
-
-            labeling.append(label)
-        
-        return labeling
 
 
 
---
+++ 1.Project/(1)_code/prediction02.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2
+import re
+import os
+from keras.models import load_model
+from class_project import Project
+
+# data
+#------------- only one file -------------------
+# path = 'D:/data/project/testset/bichon.jpg'
+# img = cv2.imread(path)
+# img = cv2.resize(img, dsize = (128, 128), interpolation = cv2.INTER_LINEAR)
+# x_pred = np.array(img/255).reshape(-1, 128, 128, 3)                 # 픽셀 = 0~255값가짐, 학습을 위해 0~1사이의 소수로 변환
+
+#---------------- load_data -------------------
+path = 'D:/data/project/testset' 
+
+x_pred = np.load('./project/project02/data/pred_image02.npy')
+                                 # filename
+print(x_pred.shape)
+
+# load_model
+model = load_model('./project/project02/model_save/best_xception.hdf5')
+
+# predict
+prediction = model.predict(x_pred)
+number = np.argmax(prediction, axis = 1)
+print(len(number))
+
+# 카테고리 불러오기
+categories = ['Bichon_frise', 'Border_collie', 'Bulldog', 'Chihuahua', 'Corgi', 'Dachshund', 'Golden_retriever', 
+                'Huskey', 'Jindo_dog', 'Maltese', 'Pug', 'Yorkshire_terrier']
+
+
+f = open('./project/project02/data/pred_image_name.txt', 'r')
+filename = f.readlines()
+
+# filename = ['Jindo_dog']
+
+for i in range(len(number)):
+    idex = number[i]
+    true = filename[i].replace('\n', '')
+    pred = categories[idex]
+    print('실제 :', true, '\t예측 견종 :', pred)
