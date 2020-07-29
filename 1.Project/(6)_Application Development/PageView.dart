4d1b7ba807a7a73650b939d1bc36a62057445203
seonukim <92.seoonooo@gmail.com>
2020-07-29 16:34:03 +0900

    commit

---
+++ 1.Project/(6)_Application Development/PageView.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}       // 공통 코드
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('제목'),
+      ),
+      body: PageView(
+        children: <Widget>[
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+          Container(
+            color: Colors.blue,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ 1.Project/(6)_Application Development/gridView.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('제목'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,      // 열수
+        children: <Widget>[
+          Container(
+            color: Colors.red,
+            width: 100,
+            height: 100,
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0),
+          ),
+          Container(
+            color: Colors.green,
+            width: 100,
+            height: 100,
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0),
+          ),
+          Container(
+            color: Colors.blue,
+            width: 100,
+            height: 100,
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0),
+          ),
+          Container(
+            color: Colors.amber,
+            width: 100,
+            height: 100,
+            padding: const EdgeInsets.all(6.0),
+            margin: const EdgeInsets.all(6.0),
+          ),
+        ],
+      ),
+    );
+  }
+}
