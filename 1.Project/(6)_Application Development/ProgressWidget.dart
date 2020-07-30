67f6cd09d91b444c00504b464a39ca58ac43ab93
seonukim <92.seoonooo@gmail.com>
2020-07-30 09:53:04 +0900

    commit

---
+++ 1.Project/(6)_Application Development/ProgressWidget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}     // 공통 코드
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Progress'),
+      ),
+      body: Align(
+        alignment: Alignment.center,
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
+
