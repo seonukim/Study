0372d7713ac09f73b34217ff62f9aa4950cca9b3
seonukim <92.seoonooo@gmail.com>
2020-08-04 18:07:13 +0900

    commit

---
+++ AE/A09_ae4_cnn.py
@@ -0,0 +1,78 @@
+# a08_ae 복붙.
+# cnn으로 오토인코더를 구성하시오
+
+import tensorflow as tf
+
+def autoencoder():
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3),
+                                     padding='same', input_shape=(28,28,1),
+                                     activation='relu')),
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2))),
+    model.add(tf.keras.layers.Conv2D(filters=16, kernel_size=(3,3),
+                                     padding='same', activation='relu')),
+    model.add(tf.keras.layers.UpSampling2D(size=(2,2))),
+    model.add(tf.keras.layers.Conv2D(filters=8, kernel_size=(3,3),
+                                     padding='same', activation='relu')),
+    model.add(tf.keras.layers.Conv2D(filters=16, kernel_size=(3,3),
+                                     padding='same', activation='relu')),
+    model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3),
+                                     padding='same', activation='relu')),
+    model.add(tf.keras.layers.Conv2D(filters=1, kernel_size=(3,3),
+                                     padding='same', activation='sigmoid'))
+    return model
+
+
+## 데이터
+train_set, test_set = tf.keras.datasets.mnist.load_data()
+x_train, y_train = train_set
+x_test, y_test = test_set
+
+x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], x_train.shape[2], 1))
+x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], x_test.shape[2], 1))
+x_train = x_train / 255.
+x_test = x_test / 255.
+print(x_train.shape)            # (60000, 28, 28, 1)
+print(x_test.shape)             # (10000, 28, 28, 1)
+
+
+model = autoencoder()
+model.summary()
+
+# model.compile(optimizer='adam', loss='mse', metrics=['acc'])  # loss = 0.01
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])    # loss = 0.09
+
+model.fit(x_train, x_train, epochs = 10)
+
+'''
+output = model.predict(x_test)
+
+from matplotlib import pyplot as plt
+import random
+fig, ((ax1, ax2, ax3, ax4, ax5), (ax6, ax7, ax8, ax9, ax10)) = \
+    plt.subplots(2, 5, figsize=(20, 7))
+
+# 이미지 다섯 개를 무작위로 고른다
+random_images = random.sample(range(output.shape[0]), 5)
+
+# 원본(입력) 이미지를 맨 위에 그린다
+for i, ax in enumerate([ax1, ax2, ax3, ax4, ax5]):
+    ax.imshow(x_test[random_images[i]], cmap='gray')
+    if i == 0:
+        ax.set_ylabel("INPUT", size=40)
+    ax.grid(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+# 오토인코더가 출력한 이미지를 아래에 그린다
+for i, ax in enumerate([ax6, ax7, ax8, ax9, ax10]):
+    ax.imshow(output[random_images[i]].reshape(28, 28), cmap='gray')
+    if i == 0:
+        ax.set_ylabel("OUTPUT", size=40)
+    ax.grid(False)
+    ax.set_xticks([])
+    ax.set.yticks([])
+
+plt.tight_layout()
+plt.show()
+'''
--- AE/A10_ae4_cnn2.py
+++ AE/A10_ae4_cnn2.py
@@ -1,44 +1,39 @@
-# a06_ae 복붙.
+# a08_ae 복붙.
+# cnn으로 오토인코더를 구성하시오
 
 import tensorflow as tf
 
-def autoencoder(hidden_1, hidden_2, hidden_3, hidden_4):
+def autoencoder():
     model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Dense(units=hidden_1, input_shape=(784,))),
-    # model.add(tf.keras.layers.BatchNormalization()),
-    # model.add(tf.keras.layers.Activation(activation='relu')),
-    # model.add(tf.keras.layers.Dropout(rate=0.3)),
-    model.add(tf.keras.layers.Dense(units=hidden_2)),
-    # model.add(tf.keras.layers.BatchNormalization()),
-    # model.add(tf.keras.layers.Activation(activation='relu')),
-    # model.add(tf.keras.layers.Dropout(rate=0.3)),
-    model.add(tf.keras.layers.Dense(units=hidden_3)),
-    # model.add(tf.keras.layers.BatchNormalization()),
-    # model.add(tf.keras.layers.Activation(activation='relu')),
-    # model.add(tf.keras.layers.Dropout(rate=0.3)),
-    model.add(tf.keras.layers.Dense(units=hidden_4))
-    model.add(tf.keras.layers.Dense(units=784)),
-    # model.add(tf.keras.layers.BatchNormalization()),
-    # model.add(tf.keras.layers.Dropout(rate=0.2)),
-    model.add(tf.keras.layers.Activation(activation='sigmoid'))
+    model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=(3,3),
+                                     padding='valid', input_shape=(28,28,1),
+                                     activation='relu')),
+    model.add(tf.keras.layers.Conv2DTranspose(filters=1, kernel_size=(3,3),
+                                              padding='valid', activation='sigmoid')),
     return model
+
 
 ## 데이터
 train_set, test_set = tf.keras.datasets.mnist.load_data()
 x_train, y_train = train_set
 x_test, y_test = test_set
 
-x_train = x_train.reshape((x_train.shape[0], x_train.shape[1] * x_train.shape[2]))
-x_test = x_test.reshape((x_test.shape[0], x_test.shape[1] * x_test.shape[2]))
+x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], x_train.shape[2], 1))
+x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], x_test.shape[2], 1))
 x_train = x_train / 255.
 x_test = x_test / 255.
+print(x_train.shape)            # (60000, 28, 28, 1)
+print(x_test.shape)             # (10000, 28, 28, 1)
 
-model = autoencoder(hidden_1=100, hidden_2=30, hidden_3=200, hidden_4=400)
+
+model = autoencoder()
+model.summary()
 
 # model.compile(optimizer='adam', loss='mse', metrics=['acc'])  # loss = 0.01
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])    # loss = 0.09
 
-model.fit(x_train, x_train, epochs = 50)
+model.fit(x_train, x_train, epochs = 10)
+
 '''
 output = model.predict(x_test)
 
