bd3771337ae69e3d9189686ff318ebd1f578f97b
seonukim <92.seoonooo@gmail.com>
2020-08-10 17:58:49 +0900

    pytorch_tutorial

--- pytorch/torch02_autograd.py
+++ pytorch/torch02_autograd.py
@@ -4,3 +4,98 @@
 # 이 패키지는 실행 시점에 정의되는 (define-by-run) 프레임워크인데
 # 다시 말하면 코드가 어떻게 실행되는지에 따라 역전파(backpropagation)이 정의되며,
 # 각각의 반복마다 역전파가 달라질 수 있다는 것이다.
+
+# 텐서(Tensors)
+# torch.Tensor는 패키지에서 가장 중심이 되는 클래스
+# 텐서의 속성 중 하나인 .requires_grad를 True로 세팅하면
+# 텐서의 모든 연산에 대하여 추적을 시작한다
+# 계산 작업이 모두 수행되었다면, .backward()를 호출하여 모든 그라디언트들을 자동으로 계산할 수 있다
+# 이 텐서를 위한 그라디언트는 .grad 속성에 누적되어 저장된다
+
+# 텐서에 대하여 기록 추적을 중지하려면 .detach()를 호출하여 현재의 계산 기록으로부터 분리시키고
+# 이후에 일어나는 계산들은 추적되지 않게 할 수 있다
+
+# 기록 추적 및 메모리 사용에 대하여 방지를 하려면
+# 코드 블럭을 with torch.no_grad(): 로 래핑할 수 있다
+# 이는 특히 모델을 평가할 때 엄청난 도움이 되는데, 왜냐하면 모델은 requires_grad = True 속성이 적용된
+# 학습 가능한 파라미터를 가지고 있을 수 있으나 우리는 그라디언트가 필요하지 않기 때문이다
+
+# 자동 미분을 위해 매우 중요한 클래스가 하나 더 있는데 바로 Function이다
+
+# Tensor와 Function은 상호 연결되어 있으며, 비순환(비주기) 그래프를 생성하는데,
+# 이 그래프는 계산 기록 전체에 대하여 인코딩을 수행한다
+# 각 변수는 Tensor를 생성한 Function을 참조하는 .grad_fn 속성을 가지고 있지만
+# 사용자에 의해 생성된 텐서는 제외한다 - 해당 텐서들은 grad_fn 자체가 None 상태이다
+
+# 만약 도함수(derivatives)를 계산하고 싶다면, Tensor의 .backward()를 호출한다
+# 만약 Tensor가 스칼라(i.e.한개의 요소를 가지고 있는) 형태라면, backward()사용에 있어
+# 그 어떠한 파라미터도 필요하지 않다
+# 그러나 한 개 이상의 요소를 가지고 있다면 올바른 모양(matching shape)의 텐서인 gradient 파라미터를 명시할 필요가 있다
+
+import torch
+
+# 텐서를 생성하고 requires_grad = True로 세팅하여 계산을 추적한다
+x = torch.ones(2, 2, requires_grad = True)
+print(x)
+
+# 텐서에 대하여 임의의 연산을 수행한다
+y = x + 2
+print(y)
+
+# y는 연산의 결과로써 생성된 것이기 때문에 grad_fn을 가지고 있다
+print(y.grad_fn)            # <AddBackward object at 0x0000023D2FF35C88>
+
+# y에 대하여 임의의 연산을 수행한다
+z = y * y * 3
+out = z.mean()
+print(z,'\n',out)
+'''
+tensor([[27., 27.],
+        [27., 27.]], grad_fn=<MulBackward>)
+tensor(27., grad_fn=<MeanBackward1>)
+'''
+
+# .requires_grad_(...)은 이미 존재하는 텐서의 requires_grad 플래그를 제자리(in-place)에서 변경한다
+# 입력 플래그를 명시하지 않은 경우 기본적으로 True가 된다
+a = torch.randn(2, 2)
+a = ((a * 3) / (a - 1))
+print(a.requires_grad)      # False
+a.requires_grad_(True)
+print(a.requires_grad)      # True
+b = (a * a).sum()
+print(b.grad_fn)            # <SumBackward0 object at 0x000002B4A1A264E0>
+
+## 그라디언트(Gradients)
+# out은 하나의 스칼라(single scalar) 값을 가지고 있기 때문에
+# out.backward()는 out.backward(torch.tensor(1))와 동등한 결과를 리턴한다
+out.backward()
+print(x.grad)
+'''
+tensor([[4.5000, 4.5000],
+        [4.5000, 4.5000]])
+'''
+
+# autograd를 이용하여 여러가지 연산 수행해보기
+x = torch.randn(3, requires_grad = True)
+
+y = x * 2
+while y.data.norm() < 1000:
+    y = y * 2
+print(y)            # tensor([605.8693, 815.9294,  66.2642], grad_fn=<MulBackward>)
+
+gradients = torch.tensor([0.1, 1.0, 0.0001], dtype=torch.float)
+y.backward(gradients)
+print(x.grad)           # tensor([ 204.8000, 2048.0000,    0.2048])
+
+# 또한 코드 블럭을 with torch.no_grad(): 로 래핑하여 requires_grad = True 속성이 명시된
+# autograd가 텐서의 추적 기록에 남지 않게 할 수 있다
+print(x.requires_grad)              # True
+print((x**2).requires_grad)         # True
+
+with torch.no_grad():
+    print((x**2).requires_grad)     # False
+
+'''
+추후 시간이 있다면 autograd와 Function에 대한 문서를 읽어보자
+http://pytorch.org/docs/stable/autograd.html
+'''
---
+++ pytorch/torch03_CNN.py
@@ -0,0 +1,83 @@
+# 뉴럴 네트워크
+# nn.Module은 여러 개의 레이어와 output을 리턴하는 forward(input) 메소드를 포함한다
+# 일반적인 뉴럴 네트워크의 학습절차는 다음과 같다
+# 학습 가능한 파라미터나 weight가 있는 뉴럴 네트워크를 정의한다
+# 입력 데이터셋에 대한 반복 학습을 진행한다
+# 네트워크를 통해 입력 값을 처리한다
+# loss를 계산한다 (loss = output - target)
+# 그라디언트를 네트워크의 파라미터로 역전파시킨다
+# 다음의 공식으로 weight를 갱신한다
+# weight = weight - learning_rate * gradient
+
+# 네트워크 정의
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # 1 input image channel, 6 output channels, 5 * 5 square convolution
+        # kernel
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # an affine operation : y = Wx + b
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    
+    def forward(self, x):
+        # MaxPooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square you can only specify a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]     # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+net = Net()
+print(net)
+'''
+Net(
+  (conv1): Conv2d(1, 6, kernel_size=(5, 5), stride=(1, 1))
+  (conv2): Conv2d(6, 16, kernel_size=(5, 5), stride=(1, 1))
+  (fc1): Linear(in_features=400, out_features=120, bias=True)       
+  (fc2): Linear(in_features=120, out_features=84, bias=True)        
+  (fc3): Linear(in_features=84, out_features=10, bias=True)
+)
+'''
+
+input = torch.randn(1, 1, 32, 32)
+out = net(input)
+print(out)
+'''
+tensor([[-0.0598, -0.1047,  0.0454, -0.0633, -0.0051, -0.0847,  0.1367,  0.0842,
+          0.0749,  0.1146]], grad_fn=<ThAddmmBackward>)
+'''
+
+# 모든 파라미터의 그라디언트 버퍼를 0으로 설정하고, 랜덤 그라디언트로 역전파를 수행한다
+net.zero_grad()
+out.backward(torch.randn(1, 10))
+
+# torch.nn은 mini-batch만을 지원한다
+# 전체 torch.nn 패키지는 mini-batch 형태인 입력만을 지원하며
+# 단일 데이터는 입력으로 지원하지 않는다
+# 예를 들어 nn.Conv2d는 nSamples x nChannels x Height x Width의 4차원 텐서를 취한다
+# 만약 단일 샘플(1개의 데이터)이 있다면, input.unsqueeze(0)를 사용하여 가짜 임시 배치 차원을 추가하면 된다
+
+# torch.Tensor - backward()와 같은 autograd 연산을 지원하는 다차원 배열이며 텐서에 대한 그라디언트를 가지고 있다
+# nn.Module - 뉴럴 네트워크 모듈로서 파라미터를 GPU로 옮기거나, 내보내기, 불러오기 등의 보조 작업을 이용하여
+# 파라미터를 캡슐화하는 편리한 방법이다
+# nn.Parameter - 텐서의 일종. Module에 속성을 할당할 때 파라미터로 자동 등록된다
+# autograd.Function - autograd 연산의 forward와 backward에 대한 정의를 구현한다
+# 모든 Tensor 연산은 최소한 하나의 Function 노드를 생성하는데, 이 노드는 Tensor를 생성하고
+# 기록을 인코딩하는 여러 함수들에 연결된다
