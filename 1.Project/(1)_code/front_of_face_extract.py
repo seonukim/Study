d0a5a58ceec06b3e462f5e9f53f5e7fed0281477
seonukim <92.seoonooo@gmail.com>
2020-08-12 17:57:43 +0900

    commit

--- 1.Project/(1)_code/front_of_face_extract.py
+++ 1.Project/(1)_code/front_of_face_extract.py
@@ -1,7 +1,11 @@
 import os
-import cv2 as cv
-path = 'C:/Users/bitcamp/Downloads/Middle_Resolution'
-os.chdir(path)
-list_dir = os.listdir(path=path)
+import cv2
+import glob
+
+image_path = 'C:/Users/bitcamp/Downloads/Middle_Resolution/middle'
+# os.chdir(image_path)
+list_dir = os.listdir(path=image_path)
 
 for i in list_dir:
+    img = image_path + '/' + i + '/S001/L1/E01/C7.jpg'
+    img = cv2.imread(img)
